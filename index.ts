ec03fdefad05afe2fd8f559da64dce3964624b9c
asadiqbal006 <165188753+asadiqbal006@users.noreply.github.com>
2024-03-28 23:34:43 +0500

    Ramdan class 04

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /user/bin/env node
+const randomNumber=Math.floor(Math.random()*6+1);
+import inquirer from "inquirer";
+const answers= await inquirer.prompt([{
+    name:"guessNumber",
+    type:"number",
+    message:"Please guess a number between 1-6",
+},]);
+if(answers.guessNumber==randomNumber){
+    console.log("Congratulation You guessed the correct number");
+    
+}
+else{
+    console.log("You guessed wrong number");
+    }
+
